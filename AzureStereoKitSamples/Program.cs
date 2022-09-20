42e39c2498bb10ca3bf87d36e85203f1ece4c945
Abdul-Mukit <abdul.mukit64@gmail.com>
2022-09-20 11:20:08 -0700

    styling, docs: Program.cs, renamed some variables for naming consistancy and added documentation. Removed unused code.

--- AzureStereoKitSamples/Program.cs
+++ AzureStereoKitSamples/Program.cs
@@ -15,23 +15,22 @@ namespace AzureStereoKitSamples
     internal class Program
     {
         // Set the scopes for API 
-        private static string[] scopesGraph = new string[] { "https://graph.microsoft.com/User.Read" };
-        private static string[] scopesStorage = new string[] { "https://storage.azure.com/user_impersonation" };
+        private static string[] _scopesGraph = new string[] { "https://graph.microsoft.com/User.Read" };
+        private static string[] _scopesStorage = new string[] { "https://storage.azure.com/user_impersonation" };
         
         // Setup the URLs
-        private static string storageAccountUrl = "https://nakamirstoragedemo1.blob.core.windows.net/";
-        private static string MSGraphURL = "https://graph.microsoft.com/v1.0/";
+        private static string _storageAccountUrl = "https://nakamirstoragedemo1.blob.core.windows.net/";
+        private static string _MSGraphURL = "https://graph.microsoft.com/v1.0/";
 
         // Below are the clientId (Application Id) of your app registration and the tenant information.
-        // You have to replace:
-        // - the content of ClientID with the Application Id for your app registration
-        private const string ClientId = "77fe1124-4d7d-497c-8440-4f8d1c36e09b";
-        private const string Tenant = "ad853c1d-e755-42bb-92fb-c8ae3c392583"; // Alternatively "[Enter your tenant, as obtained from the Azure portal, e.g. kko365.onmicrosoft.com]"
-        private const string Authority = "https://login.microsoftonline.com/" + Tenant;
+        // You have to replace the clientId and tenantId:
+        private const string _clientId = "77fe1124-4d7d-497c-8440-4f8d1c36e09b";
+        private const string _tenantId = "ad853c1d-e755-42bb-92fb-c8ae3c392583"; // Alternatively "[Enter your tenant, as obtained from the Azure portal, e.g. kko365.onmicrosoft.com]"
+        private const string _authority = "https://login.microsoftonline.com/" + _tenantId;
 
         // The MSAL Public client app
-        private static IPublicClientApplication PublicClientApp;
-        private static AuthenticationResult authResult;
+        private static IPublicClientApplication _publicClientApp;
+        
 
         private static string _resultText = String.Empty;
         private static Pose _menuPose = new Pose(Input.Head.Forward * 0.5f, Quat.LookAt(Input.Head.Forward, Input.Head.position));
@@ -57,8 +56,6 @@ namespace AzureStereoKitSamples
             Matrix floorTransform = Matrix.TS(0, -1.5f, 0, new Vec3(30, 0.1f, 30));
             Material floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
             floorMaterial.Transparency = Transparency.Blend;
-
-
             MenuState menuState = MenuState.SignedOut;
 
             // Core application loop
@@ -70,7 +67,7 @@ namespace AzureStereoKitSamples
                     if (UI.Button("Get Graph Data"))
                     {
                         menuState = MenuState.SigningIn;
-                        TryGetGraphData().ContinueWith((t) =>
+                        TryGetGraphDataAsync().ContinueWith((t) =>
                         {
                             if (t.Result)
                             {
@@ -85,7 +82,7 @@ namespace AzureStereoKitSamples
                     else if (UI.Button("Upload Blob"))
                     {
                         menuState = MenuState.SigningIn;
-                        TryUploadBlob().ContinueWith((t) =>
+                        TryUploadBlobAsync().ContinueWith((t) =>
                         {
                             if (t.Result)
                             {
@@ -110,7 +107,7 @@ namespace AzureStereoKitSamples
                 {
                     UI.Text(_resultText);
 
-                    if (UI.Button("Sing Out"))
+                    if (UI.Button("Sign Out"))
                     {
                         menuState = MenuState.SigningOut;
                         _resultText = String.Empty;
@@ -139,13 +136,19 @@ namespace AzureStereoKitSamples
             SK.Shutdown();
         }
 
-        public static async Task<bool> TryGetGraphData()
+        /// <summary>
+        /// Gets token using AAD and uses it for accessing Microsoft Graph API.
+        /// Upon successful completion, updates the _resultText which is 
+        /// later used to display the acquired data from the MS Graph API.
+        /// </summary>
+        /// <returns>bool, returns true if successfully completed.</returns>
+        public static async Task<bool> TryGetGraphDataAsync()
         {
             bool success = false;
             try
             {
                 // Sign-in user using MSAL and obtain an access token for MS Graph
-                GraphServiceClient graphClient = await SignInAndInitializeGraphServiceClient(scopesGraph);
+                GraphServiceClient graphClient = await SignInAndInitializeGraphServiceClientAsync(_scopesGraph);
 
                 // Call the /me endpoint of Graph
                 User graphUser = await graphClient.Me.Request().GetAsync();
@@ -155,7 +158,6 @@ namespace AzureStereoKitSamples
                   + "\nGiven Name: " + graphUser.GivenName + "\nid: " + graphUser.Id
                   + "\nUser Principal Name: " + graphUser.UserPrincipalName;
                 Debug.WriteLine(_resultText);
-                //DisplayBasicTokenInfo(authResult);
                 success = true;
             }
             catch (MsalException msalEx)
@@ -170,20 +172,27 @@ namespace AzureStereoKitSamples
             return success;
         }
 
-        private static async Task<bool> TryUploadBlob()
+        /// <summary>
+        /// Gets token using AAD and uses it to access Microsoft Blob Storage.
+        /// After uploading a text file to a predefined container, it then reads the content of the text file.
+        /// </summary>
+        /// <returns>bool, returns true upon successful completion.</returns>
+        private static async Task<bool> TryUploadBlobAsync()
         {
             bool success = false;
             try
             {
-                var token = await SignInUserAndGetTokenUsingMSAL(scopesStorage);
+                var token = await SignInUserAndGetTokenUsingMSAL(_scopesStorage);
                 var sb = new StringBuilder();
-                BlobServiceClient service = new BlobServiceClient(new Uri(storageAccountUrl), new StringTokenCredential(token));
+                BlobServiceClient service = new BlobServiceClient(new Uri(_storageAccountUrl), new StringTokenCredential(token));
                 BlobContainerClient container = service.GetBlobContainerClient("targetcontainer");
+                
                 if (!await container.ExistsAsync())
                 {
                     var containerResponse = await container.CreateAsync();
                     sb.AppendLine($"Created container:{container.Name} at{containerResponse.Value.LastModified}");
                 }
+                
                 var blob = container.GetBlobClient(Guid.NewGuid().ToString());
                 //if (!await blob.ExistsAsync())
                 //{
@@ -204,7 +213,7 @@ namespace AzureStereoKitSamples
             }
             catch (Exception ex)
             {
-                Debug.WriteLine($"Error {nameof(TryUploadBlob)}:{System.Environment.NewLine}{ex}");
+                Debug.WriteLine($"Error {nameof(TryUploadBlobAsync)}:{System.Environment.NewLine}{ex}");
             }
 
             return success;
@@ -214,45 +223,15 @@ namespace AzureStereoKitSamples
         /// Sign in user using MSAL and obtain a token for Microsoft Graph
         /// </summary>
         /// <returns>GraphServiceClient</returns>
-        private async static Task<GraphServiceClient> SignInAndInitializeGraphServiceClient(string[] scopes)
+        private async static Task<GraphServiceClient> SignInAndInitializeGraphServiceClientAsync(string[] scopes)
         {
-            GraphServiceClient graphClient = new GraphServiceClient(MSGraphURL,
+            GraphServiceClient graphClient = new GraphServiceClient(_MSGraphURL,
                 new DelegateAuthenticationProvider(async (requestMessage) =>
                 {
                     requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", await SignInUserAndGetTokenUsingMSAL(scopes));
                 }));
 
             return graphClient;
-        }
-
-
-        /// <summary>
-        /// Signin user.
-        /// Warning: Remember to turn on Inter Client and Internet Server & Client Capabilities
-        /// from the appmanifest.
-        /// </summary>
-        /// <param name="scopes"></param>
-        /// <returns></returns>
-        private static async Task<bool> TrySignInAsync(string[] scopes)
-        {
-            bool success = false;
-            try
-            {
-                // Sign-in user using MSAL and obtain an access token
-                await SignInUserAndGetTokenUsingMSAL(scopes);
-                success = true;
-                Debug.WriteLine("Singled in user. ");
-            }
-            catch (MsalException msalEx)
-            {
-                Debug.WriteLine($"Error Acquiring Token:{System.Environment.NewLine}{msalEx}");
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine($"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}");
-            }
-
-            return success;
         }
 
 
@@ -264,8 +243,8 @@ namespace AzureStereoKitSamples
         private static async Task<string> SignInUserAndGetTokenUsingMSAL(string[] scopes)
         {
             // Initialize the MSAL library by building a public client application
-            PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
-                .WithAuthority(Authority)
+            _publicClientApp = PublicClientApplicationBuilder.Create(_clientId)
+                .WithAuthority(_authority)
                 .WithUseCorporateNetwork(false)
                 .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
                  .WithLogging((level, message, containsPii) =>
@@ -275,21 +254,23 @@ namespace AzureStereoKitSamples
                 .Build();
 
             // It's good practice to not do work on the UI thread, so use ConfigureAwait(false) whenever possible.
-            IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(false);
+            IEnumerable<IAccount> accounts = await _publicClientApp.GetAccountsAsync().ConfigureAwait(false);
             IAccount firstAccount = accounts.FirstOrDefault();
+
+            AuthenticationResult authResult;
 
             try
             {
-                authResult = await PublicClientApp.AcquireTokenSilent(scopes, firstAccount)
+                authResult = await _publicClientApp.AcquireTokenSilent(scopes, firstAccount)
                                                   .ExecuteAsync();
             }
             catch (MsalUiRequiredException ex)
             {
-                // A MsalUiRequiredException happened on AcquireTokenSilentAsync. This indicates you need to call AcquireTokenAsync to acquire a token
+                // A MsalUiRequiredException happened on AcquireTokenSilentAsync. This indicates you need to call AcquireTokenInteractive to acquire a token
                 Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
 
 
-                authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
+                authResult = await _publicClientApp.AcquireTokenInteractive(scopes)
                                             .ExecuteAsync()
                                             .ConfigureAwait(false);
             }
@@ -301,13 +282,13 @@ namespace AzureStereoKitSamples
         /// </summary>
         private static async Task<bool> TrySignOutAsync()
         {
-            IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(false);
+            IEnumerable<IAccount> accounts = await _publicClientApp.GetAccountsAsync().ConfigureAwait(false);
             IAccount firstAccount = accounts.FirstOrDefault();
 
             bool success = false;
             try
             {
-                await PublicClientApp.RemoveAsync(firstAccount).ConfigureAwait(false);
+                await _publicClientApp.RemoveAsync(firstAccount).ConfigureAwait(false);
 
                 // user signs out dialogue
                 Debug.WriteLine("User has signed-out");
